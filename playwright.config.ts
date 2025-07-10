2fb17296e183227a79b75738758df94517c957b8
Leopold Lemmermann <leopold@lemmermann.xyz>
2025-07-10 18:41:34 +0200

    Mobile minigame and bugfixes. (#11)
    * Add apt fix.
    * Fix page selection.
    - Fix dash location in mobile layout.
    - Add npm test script.
    - Fix playwright installation.
    * Make minigame mobile-compatible(-ish).
    - Add playwright properly.

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   use: {
-    baseURL: process.env.BASE_URL || 'http://localhost:3000',
+    baseURL: process.env.BASE_URL || 'http://localhost:4321',
   },
   reporter: [['junit', { outputFile: 'test-results.xml' }]],
 });
