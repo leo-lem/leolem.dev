4027930ecd5cde84dfce44f59a12e23d7872d83d
Leopold Lemmermann <leopold@lemmermann.xyz>
2025-07-14 12:40:01 +0000

    Fix blog schema.

--- src/content/blog/index.ts
+++ src/content/blog/index.ts
@@ -1,11 +1,12 @@
 import { defineCollection, z } from "astro:content";
+import { zTag } from "../enums";
 
 export default defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
     date: z.date(),
-    tags: z.array(z.string()).optional(),
+    tags: z.array(zTag).optional(),
     draft: z.boolean().default(false),
     featured: z.boolean().default(false),
     cover: z.string().optional(),
