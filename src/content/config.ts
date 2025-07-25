09d61203c5503ae1785775dff99273d187fd91ea
Leopold Lemmermann <leopold@lemmermann.xyz>
2025-07-25 12:39:26 +0200

    Domain migration blog post. (#53)
    * Fix the mobile apps category. #42
    * Add domain blog post.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,29 @@
 import projects from "./projects";
 import stations from "./stations";
 import services from "./services";
-import topics from "./topics";
 import blog from "./blog";
+
+import { defineCollection, z } from "astro:content";
+import { file } from "astro/loaders";
+import { zIcon, zTopic } from "./zTypes";
+
+const topics = defineCollection({
+  loader: file("src/content/topics.json"),
+  schema: z.object({
+    id: zTopic,
+    icon: zIcon.optional(),
+    category: z.enum([
+      "Cloud & DevOps",
+      "AI & Data",
+      "Mobile Apps",
+      "Web Development",
+      "Community",
+      "Research"
+    ]),
+    confidence: z.number().min(0).max(100).optional(),
+    isPriority: z.boolean().default(false).optional()
+  }),
+});
 
 
 export const collections = { projects, stations, topics, services, blog };
