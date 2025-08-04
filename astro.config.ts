cec8395149821f48bd5877dd237df71fdb34d1ef
Leopold Lemmermann <81310398+leo-lem@users.noreply.github.com>
2025-08-04 20:01:39 +0200

    Https in astro config (#99)

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'http://leolem.dev',
+  site: 'https://leolem.dev',
   integrations: [tailwind(), sitemap()],
 });
