568bb27f5928570083f3d02180c2f0816e97e670
Leopold Lemmermann <81310398+leo-lem@users.noreply.github.com>
2025-08-13 12:21:20 +0000

    Align tests for trailing slash.

--- test/smoke.spec.ts
+++ test/smoke.spec.ts
@@ -6,21 +6,21 @@ test('homepage loads', async ({ page }) => {
 });
 
 test('about page loads', async ({ page }) => {
-  await page.goto('/about');
+  await page.goto('/about/');
   await expect(page.getByRole('heading', { name: "I'm Leo." })).toBeVisible();
 });
 
 test('portfolio page loads', async ({ page }) => {
-  await page.goto('/portfolio');
+  await page.goto('/portfolio/');
   await expect(page.getByRole('heading', { name: 'Portfolio' })).toBeVisible();
 });
 
 test('services page loads', async ({ page }) => {
-  await page.goto('/services');
+  await page.goto('/services/');
   await expect(page.getByRole('heading', { name: 'Services' })).toBeVisible();
 });
 
 test('explore page loads', async ({ page }) => {
-  await page.goto('/explore');
+  await page.goto('/explore/');
   await expect(page.getByRole('heading', { name: 'Explore' })).toBeVisible();
 });
