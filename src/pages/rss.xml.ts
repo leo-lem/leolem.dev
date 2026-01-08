2d3f5a04a9902c40e69c96cccd604fb095ae5fcf
Leopold Lemmermann <81310398+leo-lem@users.noreply.github.com>
2026-01-08 12:46:56 +0000

    Properly add rss.

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,14 +2,13 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export async function GET(context: { site: URL }) {
-  const posts = await getCollection("blog");
+  const posts = await getCollection("articles");
 
   return rss({
-    title: "Leo Lemmermann",
+    title: "Leopold Lemmermann",
     description: "Notes on software, freelancing, habits, and life.",
     site: context.site,
     items: posts
-      .filter((post) => !post.data.draft)
       .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf())
       .map((post) => ({
         title: post.data.title,
